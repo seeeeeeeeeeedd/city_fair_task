5200708296bbe473f9c6504fe7b04dcaac8ed657
Sophia <irisyoongi@mail.ru>
2026-08-27 12:21:58 +0700

    добавила проверку имени в классе Seller через отдельный метод

--- seller.py
+++ seller.py
@@ -1,6 +1,16 @@
 class Seller:
     def __init__(self, name: str):
-        self.__name = name
+        is_valid = self.__is_valid_name(name)
+        if is_valid:
+            self.__name = name
+        else:
+            self.__name = 'Имя неизвестно'
+
+    def __is_valid_name(self, name: str) -> bool:
+        if isinstance(name, str):
+            if name.strip():
+                return True
+        return False
 
     def get_name(self) -> str:
         return self.__name
