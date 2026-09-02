ba274c23f6237e6ef27b3bb847d1d6bfbdd05464
Sophia <irisyoongi@mail.ru>
2026-09-02 15:18:23 +0700

    исправила название класса SalePoint

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from seller import Seller
-from point_of_sale import PointOfSale
+from point_of_sale import SalePoint
 
 seller1 = Seller('Катя')
 seller2 = Seller('')
 
-point_of_sale1 = PointOfSale('Азбука вкуса')
-point_of_sale2 = PointOfSale('Восток')
+point_of_sale1 = SalePoint('Азбука вкуса')
+point_of_sale2 = SalePoint('Восток')
 
 point_of_sale1.add_seller(seller1)
 point_of_sale1.add_seller(seller2)
