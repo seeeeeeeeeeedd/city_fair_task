acf38bb84f019c5e0dbe8cf18e843baf53b63b0c
Sophia <irisyoongi@mail.ru>
2026-09-16 13:59:11 +0700

    заменила все упоминания district - теперь город напрямую хранит список всех торговых точек. добавила проверку __is_valid_sale_point для добавления торговых точек

--- city.py
+++ city.py
@@ -1,15 +1,25 @@
-from district import District
+from sale_point import SalePoint
 
 
 class City:
     def __init__(self, city_title: str):
         self.__city_title = city_title
-        self.__districts = []
+        self.__sale_points = []
 
-    def add_district(self, district: District):
-        self.__districts.append(district)
+    def add_sale_point(self, sale_point: SalePoint):
+        is_valid_sale_point = self.__is_valid_sale_point(sale_point)
+
+        if is_valid_sale_point:
+            self.__sale_points.append(sale_point)
+        else:
+            print('Добавить точку не удалось. Некорректные данные')
 
     def show_city_info(self):
         print(f'Город: {self.__city_title}')
-        for district in self.__districts:
-            district.show_district_info()
+        for sale_point in self.__sale_points:
+            sale_point.show_sellers()
+
+    def __is_valid_sale_point(self, sale_point: SalePoint) -> bool:
+        if isinstance(sale_point, SalePoint):
+            return True
+        return False
